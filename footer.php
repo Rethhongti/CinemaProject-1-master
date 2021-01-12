ffdff4b56e01be12bbc86897dc7bf65bc0bf231d
Fanna///7 <fannasaran@gmail.com>
2021-01-12 12:29:41 +0700

    updated

--- footer.php
+++ footer.php
@@ -2,9 +2,11 @@
    <div id="footer">
        <div>
            <p>CONTACT US</p>
+           
        </div>
        <div>
            <p>ABOUT</p>
+           <p>This is a website to booking the cinema seat in legend.</p> 
        </div>
        <div>
            <p>DEVELOPER</p>
